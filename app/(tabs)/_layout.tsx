a7332f13cc9123b936cb443290d4ac94a77d0cca
camsandvl <san24358@uvg.edu.gt>
2026-04-05 00:22:28 -0600

    Add modal added to navigation bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,27 @@
-import { Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 
 export default function TabsLayout() {
+  const router = useRouter();
+
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="search" options={{ title: "Search" }} />
+
+      {/* ADD BUTTON (no tab) */}
+      <Tabs.Screen
+        name="add"
+        options={{
+          title: "Add",
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault(); // evita navegación normal
+            router.push("/(add)/camera"); // abre modal
+          },
+        }}
+      />
+
       <Tabs.Screen name="challenges" options={{ title: "Challenges" }} />
       <Tabs.Screen name="profile" options={{ title: "Profile" }} />
     </Tabs>
