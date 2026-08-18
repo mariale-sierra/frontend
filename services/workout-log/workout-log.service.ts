870006e1ec206a5784279e5527409bc253429847
mariale-sierra <sie24405@uvg.edu.gt>
2026-08-17 19:15:07 -0600

    elimina createWorkoutLog/getWorkoutLog sin uso
    quedaron huerfanos al sacar el submitMetrics roto; createWorkoutLog
    ademas nunca funciono (el endpoint POST /workout-logs exige imagen o
    isRestDay y este wrapper no mandaba ninguno de los dos).

--- services/workout-log/workout-log.service.ts
+++ services/workout-log/workout-log.service.ts
@@ -1,19 +1,6 @@
 import api from '../api';
-import type {
-  CreateWorkoutLogRequest,
-  WorkoutLogContract,
-} from '../../types/workout-log';
+import type { WorkoutLogContract } from '../../types/workout-log';
 import type { ProgressSubmissionRequest } from '../../types/challenge';
-
-export async function createWorkoutLog(data: CreateWorkoutLogRequest) {
-  const response = await api.post<WorkoutLogContract>('/workout-logs', data);
-  return response.data;
-}
-
-export async function getWorkoutLog(id: number) {
-  const response = await api.get<WorkoutLogContract>(`/workout-logs/${id}`);
-  return response.data;
-}
 
 export async function submitWorkoutProgress(
   data: ProgressSubmissionRequest,
